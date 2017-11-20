696132e11c940631f84c730132b4f410aaba1260
Patrick <pattuga@gmail.com>
2017-11-20 01:19:57 +0000

    Add skeleton

---
+++ proj1/pagerank-nibble.py
@@ -0,0 +1,9 @@
+import networkx as nx
+
+def pagerank_nibble(graph, initial_vertex, connectivity, volume):
+    """
+    """
+    alpha = connectivity
+    pagerank_vector = aproximate_pagerank(graph)
+
+def aproximate_pagerank(graph, starting_vector, prob_teleport, aprox_ratio):

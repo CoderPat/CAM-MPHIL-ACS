ca9b9cf66e3327d6962dab0e8fd3d89e90216ae2
CoderPat <pattuga@gmail.com>
2018-02-26 22:39:30 +0000

    Fix baseline and add saved model

--- mini-project/gaussiannets.py
+++ mini-project/gaussiannets.py
@@ -74,13 +74,12 @@ class ProbabilisticCNN:
                                epochs=self.__epochs,
                                verbose=1)
 
-
         features_train = self.__features_extractor.predict(x_train).astype(np.float64)
         with self.__gclassifier_graph.as_default():
             kernel = gpflow.kernels.Matern32(input_dim=128) + gpflow.kernels.White(input_dim=128, variance=0.01)
             self.__gaussian_classifier = gpflow.models.SVGP(features_train, y_train, kernel,
                                                             likelihood=gpflow.likelihoods.MultiClass(10),
-                                                            Z=features_train[::600].copy(), num_latent=10, whiten=True, q_diag=True)
+                                                            Z=features_train[::300].copy(), num_latent=10, whiten=True, q_diag=True)
 
             self.__gaussian_classifier.kern.white.variance.trainable = False
             self.__gaussian_classifier.feature.trainable = False
@@ -88,25 +87,25 @@ class ProbabilisticCNN:
             opt.minimize(self.__gaussian_classifier)
         
         if self.__raw_gpc:
-            with self.__raw_gpc_graph.as_default():
-                flatten_x = x_train.reshape(x_train.shape[0], -1)
-                kernel = gpflow.kernels.Matern32(input_dim=128) + gpflow.kernels.White(input_dim=128, variance=0.01)
+            with self.__gclassifier_graph.as_default():
+                flatten_x = x_train.reshape(x_train.shape[0], -1).astype(np.float64)
+                kernel = gpflow.kernels.Matern32(input_dim=flatten_x.shape[1]) + gpflow.kernels.White(input_dim=flatten_x.shape[1], variance=0.01)
                 self.__raw_gpc = gpflow.models.SVGP(flatten_x, y_train, kernel,
                                                     likelihood=gpflow.likelihoods.MultiClass(10),
-                                                    Z=features_train[::600].copy(), num_latent=10, whiten=True, q_diag=True)
+                                                    Z=flatten_x[::300].copy(), num_latent=10, whiten=True, q_diag=True)
 
                 self.__raw_gpc.kern.white.variance.trainable = False
-                self.__raw_gcp.feature.trainable = False
+                self.__raw_gpc.feature.trainable = False
                 opt = gpflow.train.ScipyOptimizer()
-                opt.minimize(self.__raw_gcp)
+                opt.minimize(self.__raw_gpc)
 
     def predict(self, x, raw_predict=False):
         """
         """
         if raw_predict:
-            if self.__raw_gcp is not None:
-                flatten_x = x_train.reshape(x_train.shape[0], -1)
-                return self.__network.predict(x), self.__raw_gcp.predict_y(flatten_x)
+            if self.__raw_gpc is not None:
+                flatten_x = x.reshape(x.shape[0], -1)
+                return self.__network.predict(x), self.__raw_gpc.predict_y(flatten_x)
             else:
                 return self.__network.predict(x)
         else:
@@ -117,17 +116,17 @@ class ProbabilisticCNN:
     def save(self, path):
         network_weights = self.__network.get_weights()
         gp_weights = self.__gaussian_classifier.read_trainables()
-        if self.__raw_gcp is not None:
-            raw_gp_weigths = self.__raw_gcp.read_trainables()
+        if self.__raw_gpc is not None:
+            raw_gpc_weights = self.__raw_gpc.read_trainables()
         else:
-            raw_gp_weigths = None
+            raw_gpc_weights = None
 
         with open(path, 'wb') as f:
-            pickle.dump((network_weights, gp_weights, raw_gp_weigths), f)
+            pickle.dump((network_weights, gp_weights, raw_gpc_weights), f)
 
     def load(self, path, x_train, y_train_m):
         with open(path, 'rb') as f:
-            network_weights, gp_weights, raw_gp_weigths = pickle.load(f)
+            network_weights, gp_weights, raw_gp_weights = pickle.load(f)
 
         self.__network.set_weights(network_weights)
         features_train = self.__features_extractor.predict(x_train).astype(np.float64)
@@ -137,20 +136,21 @@ class ProbabilisticCNN:
             kernel = gpflow.kernels.Matern32(input_dim=128) + gpflow.kernels.White(input_dim=128, variance=0.01)
             self.__gaussian_classifier = gpflow.models.SVGP(features_train, y_train, kernel,
                                                             likelihood=gpflow.likelihoods.MultiClass(10),
-                                                            Z=features_train[::600].copy(), num_latent=10, whiten=True, q_diag=True)
+                                                            Z=features_train[::300].copy(), num_latent=10, whiten=True, q_diag=True)
 
             self.__gaussian_classifier.kern.white.variance.trainable = False
             self.__gaussian_classifier.feature.trainable = False
             self.__gaussian_classifier.assign(gp_weights)
 
-        if raw_gp_weigths is not None:
-            flatten_x = x_train.reshape(x_train.shape[0], -1)
-            kernel = gpflow.kernels.Matern32(input_dim=128) + gpflow.kernels.White(input_dim=128, variance=0.01)
-            self.__raw_gpc = gpflow.models.SVGP(features_train, y_train, kernel,
-                                                            likelihood=gpflow.likelihoods.MultiClass(10),
-                                                            Z=features_train[::600].copy(), num_latent=10, whiten=True, q_diag=True)
+        if raw_gp_weights is not None:
+            with self.__gclassifier_graph.as_default():
+                flatten_x = x_train.reshape(x_train.shape[0], -1).astype(np.float64)
+                kernel = gpflow.kernels.Matern32(input_dim=flatten_x.shape[1]) + gpflow.kernels.White(input_dim=flatten_x.shape[1], variance=0.01)
+                self.__raw_gpc = gpflow.models.SVGP(features_train, y_train, kernel,
+                                                    likelihood=gpflow.likelihoods.MultiClass(10),
+                                                    Z=flatten_x[::300].copy(), num_latent=10, whiten=True, q_diag=True)
 
-            self.__raw_gpc.kern.white.variance.trainable = False
-            self.__raw_gpc.feature.trainable = False
-            self.__raw_gpc.assign(gp_weights)
+                self.__raw_gpc.kern.white.variance.trainable = False
+                self.__raw_gpc.feature.trainable = False
+                self.__raw_gpc.assign(raw_gp_weights)
 

8ca4f2fcda896cfd1da20d7117f859f0c03703e4
CoderPat <pattuga@gmail.com>
2018-03-21 01:04:36 +0000

    Change model architecture and add better serialization

--- graph2sequence/others/classification_gnn.py
+++ graph2sequence/others/classification_gnn.py
@@ -82,19 +82,11 @@ class ClassificationGNN(BaseEmbeddingsGNN):
             self.accuracy = [tf.reduce_mean(tf.cast(correct_prediction, tf.float32))]
         return [self.accuracy]
 
-    def get_accuracy(self, extra_results): 
-        results, num_graphs = zip(*extra_results)
+    def get_log(self, loss, speed, num_graphs, extra_results, _):
         accuracies = np.array([result[0] for result in results]) * np.array(num_graphs)
-        return np.sum(accuracies)/np.sum(np.array(num_graphs))
-
-    def get_train_log(self, loss, speed, extra_results):
-        accuracy = self.get_accuracy(extra_results)
+        accuracy = np.sum(accuracies)/np.sum(np.array(num_graphs))
         return "Train: loss: %.5f | acc: %.5f | instances/sec: %.2f", (loss, accuracy, speed)
     
-    def get_valid_log(self, loss, speed, extra_results):
-        accuracy = self.get_accuracy(extra_results)
-        return "Valid: loss: %.5f | acc: %.5f | instances/sec: %.2f", (loss, accuracy, speed)
-        
 
 def main():
     try:
--- graph2sequence/sequence_gnn.py
+++ graph2sequence/sequence_gnn.py
@@ -22,6 +22,7 @@ import tensorflow as tf
 import sys, traceback
 import pdb
 
+from tensorflow.contrib.learn import ModeKeys
 from scipy.sparse import vstack, csr_matrix
 from .base.base_embeddings_gnn import BaseEmbeddingsGNN
 from .base.utils import glorot_init, MLP, compute_bleu
@@ -31,7 +32,6 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
     """
     def __init__(self, args):
         super().__init__(args)
-        self.batch_raw_targets = []
         self.max_output_len = 0
         self.target_vocab_size = 0
 
@@ -60,13 +60,13 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
         super().prepare_specific_graph_model()
 
 
-    def process_data(self, data, is_training_data: bool):
+    def process_data(self, data, mode):
         restrict = self.args.get("--restrict_data")
         if restrict is not None and restrict > 0:
             data = data[:restrict]
 
         # Get some common data out:
-        if self.params['input_shape'] is None or self.params['target_vocab_size'] is None:
+        if mode == ModeKeys.TRAIN and (self.params['input_shape'] is None or self.params['target_vocab_size'] is None):
             num_fwd_edge_types = 0
             for g in data:
                 self.max_num_vertices = max(self.max_num_vertices, max([v for e in g['graph'] for v in [e[0], e[2]]]))
@@ -83,43 +83,47 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
             self.eos_symbol = self.target_vocab_size + 1
             self.pad_symbol = self.target_vocab_size + 2
 
-        return self.process_raw_graphs(data, is_training_data)
+        return self.process_raw_graphs(data, mode)
 
-    def process_raw_graphs(self, raw_data: Sequence[Any], is_training_data: bool) -> Any:
+    def process_raw_graphs(self, raw_data: Sequence[Any], mode) -> Any:
         processed_graphs = []
         for d in raw_data:
             (adjacency_lists, num_incoming_edge_per_type) = self._BaseEmbeddingsGNN__graph_to_adjacency_lists(d['graph'])
-            sequence_len = d['output'].shape[0] + 1
-            pad_size = self.max_output_len - sequence_len
-
-            decoder_input = np.concatenate((np.array([self.go_symbol]), 
-                                            d['output'],
-                                            np.array([self.eos_symbol]),
-                                            np.array(pad_size * [self.pad_symbol])))
-
             processed_graphs.append({"adjacency_lists": adjacency_lists,
-                                     "num_incoming_edge_per_type": num_incoming_edge_per_type,
-                                     "init": d["node_features"],
-                                     "targets": decoder_input[1:],
-                                     "raw_targets": d['output'],
-                                     "decoder_inputs": decoder_input,
-                                     "sequence_len": sequence_len})
+                                        "num_incoming_edge_per_type": num_incoming_edge_per_type,
+                                        "init": d["node_features"]})
+
+            if mode != ModeKeys.INFER:
+                sequence_len = d['output'].shape[0] + 1
+                pad_size = self.max_output_len - sequence_len
+
+                decoder_input = np.concatenate((np.array([self.go_symbol]), 
+                                                d['output'],
+                                                np.array([self.eos_symbol]),
+                                                np.array(pad_size * [self.pad_symbol])))
+
+                processed_graphs[-1].update({"targets": decoder_input[1:],
+                                             "raw_targets": d['output'],
+                                             "decoder_inputs": decoder_input,
+                                             "sequence_len": sequence_len})
 
         return processed_graphs
 
-    def make_minibatch_iterator(self, data: Any, is_training: bool):
+    def make_minibatch_iterator(self, data: Any, mode):
         """Create minibatches by flattening adjacency matrices into a single adjacency matrix with
         multiple disconnected components."""
-        if is_training:
+        if mode == ModeKeys.TRAIN:
             np.random.shuffle(data)
+        elif mode == ModeKeys.EVAL:
+            self.raw_targets = [graph['raw_targets'] for graph in data]
+
         # Pack until we cannot fit more graphs in the batch
-        dropout_keep_prob = self.params['graph_state_dropout_keep_prob'] if is_training else 1.
+        dropout_keep_prob = self.params['graph_state_dropout_keep_prob'] if mode == ModeKeys.TRAIN else 1.
         num_graphs = 0
         while num_graphs < len(data):
             num_graphs_in_batch = 0
             batch_node_features = csr_matrix(np.empty(shape=(0,data[0]['init'].shape[1])))
             batch_target_task_values = []
-            batch_raw_targets = []
             batch_decoder_inputs = []
             batch_sequence_lens = []
             batch_target_mask = []
@@ -145,28 +149,40 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
                         num_incoming_edges_per_type[node_id, e_type] = edge_count
                 batch_num_incoming_edges_per_type.append(num_incoming_edges_per_type)
 
-                batch_target_task_values.append(cur_graph['targets'])
-                batch_raw_targets.append(cur_graph['raw_targets'])
-                batch_decoder_inputs.append(cur_graph['decoder_inputs'])
-                batch_sequence_lens.append(cur_graph['sequence_len'])
-                batch_target_mask.append(cur_graph['sequence_len'] * [1] + (self.max_output_len - cur_graph['sequence_len'])*[0])
+                if mode != ModeKeys.INFER:
+                    batch_target_task_values.append(cur_graph['targets'])
+                    batch_decoder_inputs.append(cur_graph['decoder_inputs'])
+                    batch_sequence_lens.append(cur_graph['sequence_len'])
+                    batch_target_mask.append(cur_graph['sequence_len'] * [1] + (self.max_output_len - cur_graph['sequence_len'])*[0])
+
                 num_graphs += 1
                 num_graphs_in_batch += 1
                 node_offset += num_nodes_in_graph
 
             batch_node_features = batch_node_features.tocoo()
             indices = np.array([[row] for row in batch_node_features.row])
-            batch_feed_dict = {
-                self.placeholders['initial_node_representation']: (indices, batch_node_features.col, (batch_node_features.shape[0], )),
-                self.placeholders['num_incoming_edges_per_type']: np.concatenate(batch_num_incoming_edges_per_type, axis=0),
-                self.placeholders['graph_nodes_list']: np.array(batch_graph_nodes_list, dtype=np.int32),
-                self.placeholders['target_values']: np.array(batch_target_task_values),
-                self.placeholders['decoder_inputs']: np.array(batch_decoder_inputs),
-                self.placeholders['sequence_lens']: np.array(batch_sequence_lens),
-                self.placeholders['target_mask'] : np.array(batch_target_mask),
-                self.placeholders['num_graphs']: num_graphs_in_batch,
-                self.placeholders['graph_state_keep_prob']: dropout_keep_prob,
-            }
+
+            if mode == ModeKeys.INFER:
+                batch_feed_dict = {
+                    self.placeholders['initial_node_representation']: (indices, batch_node_features.col, (batch_node_features.shape[0], )),
+                    self.placeholders['num_incoming_edges_per_type']: np.concatenate(batch_num_incoming_edges_per_type, axis=0),
+                    self.placeholders['graph_nodes_list']: np.array(batch_graph_nodes_list, dtype=np.int32),
+                    self.placeholders['num_graphs']: num_graphs_in_batch,
+                    self.placeholders['graph_state_keep_prob']: dropout_keep_prob,
+                }
+
+            else:
+                batch_feed_dict = {
+                    self.placeholders['initial_node_representation']: (indices, batch_node_features.col, (batch_node_features.shape[0], )),
+                    self.placeholders['num_incoming_edges_per_type']: np.concatenate(batch_num_incoming_edges_per_type, axis=0),
+                    self.placeholders['graph_nodes_list']: np.array(batch_graph_nodes_list, dtype=np.int32),
+                    self.placeholders['target_values']: np.array(batch_target_task_values),
+                    self.placeholders['decoder_inputs']: np.array(batch_decoder_inputs),
+                    self.placeholders['sequence_lens']: np.array(batch_sequence_lens),
+                    self.placeholders['target_mask'] : np.array(batch_target_mask),
+                    self.placeholders['num_graphs']: num_graphs_in_batch,
+                    self.placeholders['graph_state_keep_prob']: dropout_keep_prob,
+                }
 
             # Merge adjacency lists and information about incoming nodes:
             for i in range(self.num_edge_types):
@@ -176,8 +192,6 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
                     adj_list = np.zeros((0, 2), dtype=np.int32)
                 batch_feed_dict[self.placeholders['adjacency_lists'][i]] = adj_list
 
-            if not is_training:
-                self.batch_raw_targets.extend(batch_raw_targets)
             yield batch_feed_dict
 
     def get_output(self, last_h):
@@ -185,7 +199,7 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
 
         self.projection_layer = tf.layers.Dense(self.target_vocab_size+3)
 
-        self.weights['output_embeddings'] = tf.Variable(glorot_init([self.target_vocab_size, h_dim]),
+        self.weights['output_embeddings'] = tf.Variable(glorot_init([self.target_vocab_size+3, h_dim]),
                                                                      name='output_embeddings')
 
         activation_name = self.params['graph_rnn_activation'].lower()
@@ -197,8 +211,6 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
         cell_type = self.params['decoder_rnn_cell'].lower()
         if cell_type == 'gru':
             self.decoder_cell = tf.nn.rnn_cell.GRUCell(h_dim, activation=activation_fun)
-        elif cell_type == 'lstm':
-            self.decoder_cell = tf.nn.rnn_cell.BasicLSTMCell(h_dim, activation=activation_fun)
         elif cell_type == 'rnn':
             self.decoder_cell = tf.nn.rnn_cell.BasicRNNCell(h_dim, activation=activation_fun)
 
@@ -212,17 +224,30 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
 
         decoder_emb_inp = tf.nn.embedding_lookup(self.weights['output_embeddings'], 
                                                  self.placeholders['decoder_inputs'])
+
+        with tf.variable_scope("train_decoder"):
+            train_helper = tf.contrib.seq2seq.TrainingHelper(decoder_emb_inp, 
+                                                            self.placeholders['sequence_lens'], 
+                                                            time_major=False)
+
+            train_decoder = tf.contrib.seq2seq.BasicDecoder(self.decoder_cell, 
+                                                            train_helper, self.ops['graph_embeddings'],
+                                                            output_layer=self.projection_layer)
+
+            train_outputs, _, _= tf.contrib.seq2seq.dynamic_decode(train_decoder)
         
-        #For Training
-        train_helper = tf.contrib.seq2seq.TrainingHelper(decoder_emb_inp, 
-                                                         self.placeholders['sequence_lens'], 
-                                                         time_major=False)
+        with tf.variable_scope("infer_decoder"):
+            start_tokens = tf.fill([self.placeholders['num_graphs']], tf.cast(self.go_symbol, tf.int32))
+            infer_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.weights['output_embeddings'], 
+                                                                    start_tokens, self.eos_symbol)
+   
+            infer_decoder = tf.contrib.seq2seq.BasicDecoder(self.decoder_cell, 
+                                                            infer_helper, self.ops['graph_embeddings'],
+                                                            output_layer=self.projection_layer)
 
-        train_decoder = tf.contrib.seq2seq.BasicDecoder(self.decoder_cell, 
-                                                        train_helper, self.ops['graph_embeddings'],
-                                                        output_layer=self.projection_layer)
+            self.ops['infer_output'], _, _= tf.contrib.seq2seq.dynamic_decode(infer_decoder,
+                                                                              maximum_iterations=self.params['max_output_len'])
 
-        train_outputs, _, _= tf.contrib.seq2seq.dynamic_decode(train_decoder)
 
         return train_outputs.rnn_output
 
@@ -235,32 +260,27 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
 
 
     def get_extra_valid_ops(self, computed_logits, _ ):
-        start_tokens = tf.fill([self.placeholders['num_graphs']], tf.cast(self.go_symbol, tf.int32))
-        infer_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.weights['output_embeddings'], 
-                                                                start_tokens, self.eos_symbol)
-   
-        infer_decoder = tf.contrib.seq2seq.BasicDecoder(self.decoder_cell, 
-                                                        infer_helper, self.ops['graph_embeddings'],
-                                                        output_layer=self.projection_layer)
+        return [self.ops['infer_output'].sample_id]
 
-        infer_outputs, _, _= tf.contrib.seq2seq.dynamic_decode(infer_decoder,
-                                                               maximum_iterations=self.params['max_output_len'])
+    def get_inference_ops(self, computed_logits):
+        return [self.ops['infer_output'].sample_id]
 
-        return [infer_outputs.sample_id]
+    def get_log(self, loss, speed, _ , extra_results, mode):
+        if mode == ModeKeys.TRAIN:
+            return "loss : %.5f | instances/sec: %.2f", (loss, speed)
+        elif mode == ModeKeys.EVAL:
+            #Extra sampled sentences from results
+            sampled_ids = [sampled_sentence.tolist() for result in extra_results for sampled_sentence in result[0]]
 
-        
-    def get_valid_log(self, loss, speed, extra_results):
-        #Extra sampled sentences from results
-        results, _ = zip(*extra_results)
-        sampled_ids = [sampled_sentence.tolist() for result in results for sampled_sentence in result[0]]
+            #Calculate BLEU
+            reference_corpus = [[reference.tolist()] for reference in self.raw_targets]
+            sampled_sentences = self.get_translations(sampled_ids)
+            bleu = compute_bleu(reference_corpus, sampled_sentences, max_order=1)[0]
 
-        #Calculate BLEU
-        reference_corpus = [[reference.tolist()] for reference in self.batch_raw_targets]
-        sampled_sentences = self.get_translations(sampled_ids)
-        bleu = compute_bleu(reference_corpus, sampled_sentences, max_order=1)[0]
+            return "loss: %.5f BLEU: %.5f | instances/sec: %.2f", (loss, bleu, speed)
 
-        return "Valid: loss: %.5f BLEU: %.5f | instances/sec: %.2f", (loss, bleu, speed)
-
+    def get_checkpoint_metric(self, valid_log):
+        return valid_log[1]
     
     def get_translations(self, sampled_ids):
         sentences = []
@@ -270,8 +290,7 @@ class Graph2SequenceGNN(BaseEmbeddingsGNN):
             sentences.append(sampled_sentence)
         return sentences
 
- 
-
+        
 
 def main():
     try:
